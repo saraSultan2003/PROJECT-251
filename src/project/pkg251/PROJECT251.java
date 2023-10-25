3e5c2975616068bc3407d0cc882f025ca8ead072
saraSultan2003 <143970178+saraSultan2003@users.noreply.github.com>
2023-10-25 20:36:21 +0300

    Update PROJECT251.java

--- src/project/pkg251/PROJECT251.java
+++ src/project/pkg251/PROJECT251.java
@@ -16,6 +16,7 @@ public class PROJECT251 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         System.out.print("hello world");
     }
     
 }
